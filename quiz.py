7e56417da1b4a90f1dbec4183efacb29bedfe965
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-17 17:42:26 +0600

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,34 +1,71 @@
-from random import randint
-from flask import Flask, session, redirect, url_for
-from db_scripts import get_question_after
+from flask import Flask, session, request, redirect, url_for
+from db_scripts import get_question_after, get_quises
+ 
+def start_quis(quiz_id):
 
-quiz = 0
-last_question = 0
+    session['quiz'] = quiz_id
+    session['last_question'] = 0
+ 
+def end_quiz():
+    session.clear()
+ 
+def quiz_form():
+   
+    html_beg = '''<html><body><h2>Выберите викторину:</h2><form method="post" action="index"><select name="quiz">'''
+    frm_submit = '''<p><input type="submit" value="Выбрать"> </p>'''
 
+    html_end = '''</select>''' + frm_submit + '''</form></body></html>'''
+    options = ''' '''
+    q_list = get_quises()
+    for id, name in q_list:
+        option_line = ('''<option value="''' +
+                        str(id) + '''">''' +
+                        str(name) + '''</option>
+                      ''')
+        options = options + option_line
+    return html_beg + options + html_end
+       
 def index():
-   global quiz, last_question
-   max_quiz = 3
-   quiz = randint(1, max_quiz)
-   last_question = 0
-   return '<a href="/test">Тест</a>'
-
+    ''' Первая страница: если пришли запросом GET, то выбрать викторину, 
+    если POST - то запомнить id викторины и отправлять на вопросы'''
+    if request.method == 'GET':
+        # викторина не выбрана, сбрасываем id викторины и показываем форму выбора
+        start_quis(-1)
+        return quiz_form()
+    else:
+        # получили дополнительные данные в запросе! Используем их:
+        quest_id = request.form.get('quiz') # выбранный номер викторины 
+        start_quis(quest_id)
+        return redirect(url_for('test'))
+ 
 def test():
-   global last_question
-   result = get_question_after(last_question, quiz)
-   if result is None or len(result) == 0:   
-           return redirect(url_for('result'))
-
-   else:
-       last_question = result[0]
-       return '<h1>' + str(quiz) + '<br>' + str(result) + '</h1>'
-
+    '''возвращает страницу вопроса'''
+    # что если пользователь без выбора викторины пошел сразу на адрес '/test'? 
+    if not ('quiz' in session) or int(session['quiz']) < 0:
+        return redirect(url_for('index'))
+    else:
+        # тут пока старая версия функции:
+        result = get_question_after(session['last_question'], session['quiz'])
+        if result is None or len(result) == 0:
+            return redirect(url_for('result'))
+        else:
+            session['last_question'] = result[0]
+            # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
+            return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
+ 
 def result():
-   return "that's all folks!"
-
+    end_quiz()
+    return "that's all folks!"
+ 
+# Создаём объект веб-приложения:
 app = Flask(__name__)  
-app.add_url_rule('/', 'index', index)
-app.add_url_rule('/test', 'test', test)
-app.add_url_rule('/result', 'result', result)
-
-if __name__ == '__main__':
-   app.run()
+app.add_url_rule('/', 'index', index)   # создаёт правило для URL '/'
+app.add_url_rule('/index', 'index', index, methods=['post', 'get']) # правило для '/index' 
+app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
+app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
+# Устанавливаем ключ шифрования:
+app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
+ 
+if __name__ == "__main__":
+    # Запускаем веб-сервер:
+    app.run()
