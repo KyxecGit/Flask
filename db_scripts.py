4138bbe0cecfa3a31e1c842e093f35e3c20cc7fe
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-19 17:29:32 +0600

    Add files via upload

--- db_scripts.py
+++ db_scripts.py
@@ -1,25 +1,27 @@
 import sqlite3
-from random import randint
- 
 db_name = 'quiz.sqlite'
 conn = None
-cursor = None
- 
+curor = None
+
+
 def open():
     global conn, cursor
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
- 
+
+
 def close():
     cursor.close()
     conn.close()
- 
+
+
 def do(query):
     cursor.execute(query)
     conn.commit()
- 
+
+
 def clear_db():
-    ''' убивает все таблицы '''
+    ''' удаляет все таблицы '''
     open()
     query = '''DROP TABLE IF EXISTS quiz_content'''
     do(query)
@@ -28,70 +30,62 @@ def clear_db():
     query = '''DROP TABLE IF EXISTS quiz'''
     do(query)
     close()
- 
+    
 def create():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
-    
+
+
     do('''CREATE TABLE IF NOT EXISTS quiz (
-            id INTEGER PRIMARY KEY, 
-            name VARCHAR)''' 
-    )
+           id INTEGER PRIMARY KEY,
+           name VARCHAR)''')
+
+
     do('''CREATE TABLE IF NOT EXISTS question (
-                id INTEGER PRIMARY KEY, 
-                question VARCHAR, 
-                answer VARCHAR, 
-                wrong1 VARCHAR, 
-                wrong2 VARCHAR, 
-                wrong3 VARCHAR)'''
-    )
+               id INTEGER PRIMARY KEY,
+               question VARCHAR,
+               answer VARCHAR,
+               wrong1 VARCHAR,
+               wrong2 VARCHAR,
+               wrong3 VARCHAR)''')
+
+
     do('''CREATE TABLE IF NOT EXISTS quiz_content (
-                id INTEGER PRIMARY KEY,
-                quiz_id INTEGER,
-                question_id INTEGER,
-                FOREIGN KEY (quiz_id) REFERENCES quiz (id),
-                FOREIGN KEY (question_id) REFERENCES question (id) )'''
-    )
+               id INTEGER PRIMARY KEY,
+               quiz_id INTEGER,
+               question_id INTEGER,
+               FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+               FOREIGN KEY (question_id) REFERENCES question (id) )''')
     close()
- 
-def show(table):
-    query = 'SELECT * FROM ' + table
-    open()
-    cursor.execute(query)
-    print(cursor.fetchall())
-    close()
- 
-def show_tables():
-    show('question')
-    show('quiz')
-    show('quiz_content')
- 
+
+
 def add_questions():
     questions = [
         ('Сколько месяцев в году имеют 28 дней?', 'Все', 'Один', 'Ни одного', 'Два'),
-        ('Каким станет зелёный утёс, если упадет в Красное море?', 'Мокрым?', 'Красным', 'Не изменится', 'Фиолетовым'),
+        ('Каким станет зелёный утёс, если упадёт в Красное море?', 'Мокрым', 'Красным', 'Не изменится', 'Фиолетовым'),
         ('Какой рукой лучше размешивать чай?', 'Ложкой', 'Правой', 'Левой', 'Любой'),
         ('Что не имеет длины, глубины, ширины, высоты, а можно измерить?', 'Время', 'Глупость', 'Море', 'Воздух'),
         ('Когда сетью можно вытянуть воду?', 'Когда вода замерзла', 'Когда нет рыбы', 'Когда уплыла золотая рыбка', 'Когда сеть порвалась'),
-        ('Что больше слона и ничего не весит?', 'Тень слона', 'Воздушный шар', 'Парашют', 'Облако'),
-        ('Что такое у меня в кармашке?', 'Кольцо', 'Кулак', 'Дырка', 'Бублик')
+        ('Что больше слона и ничего не весит?', 'Тень слона', 'Воздушный шар', 'Парашют', 'Облако')
     ]
     open()
     cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''', questions)
     conn.commit()
     close()
- 
+
+
 def add_quiz():
     quizes = [
-        ('Викторина 1', ),
-        ('Викторина 2', ),
-        ('Викторина-непоймикакая', )
+        ('Своя игра', ),
+        ('Кто хочет стать миллионером?', ),
+        ('Самый умный', )
     ]
     open()
     cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
     conn.commit()
     close()
- 
+
+
 def add_links():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
@@ -104,64 +98,48 @@ def add_links():
         conn.commit()
         answer = input("Добавить связь (y / n)?")
     close()
- 
- 
-def get_question_after(last_id=0, vict_id=1):
+
+
+def show(table):
+    query = 'SELECT * FROM ' + table
+    open()
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+
+
+def show_tables():
+    show('question')
+    show('quiz')
+    show('quiz_content')
+
+
+def get_question_after(question_id = 0, quiz_id=1):
     ''' возвращает следующий вопрос после вопроса с переданным id
     для первого вопроса передаётся значение по умолчанию '''
     open()
     query = '''
     SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
-    FROM question, quiz_content 
-    WHERE quiz_content.question_id == question.id
-    AND quiz_content.id > ? AND quiz_content.quiz_id == ? 
+    FROM question, quiz_content
+    WHERE quiz_content.question_id == question.id 
+    AND quiz_content.id > ? AND quiz_content.quiz_id == ?
     ORDER BY quiz_content.id '''
-    cursor.execute(query, [last_id, vict_id] )
- 
-    result = cursor.fetchone()
-    close()
-    return result 
- 
-def get_quises():
-    ''' возвращает список викторин (id, name) 
-    можно брать только викторины, в которых есть вопросы, но пока простой вариант '''
-    query = 'SELECT * FROM quiz ORDER BY id'
-    open()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    close()
-    return result
- 
-def get_quiz_count():
-    ''' необязательная функция '''
-    query = 'SELECT MAX(quiz_id) FROM quiz_content'
-    open()
-    cursor.execute(query)
+    cursor.execute(query, [question_id, quiz_id] )
     result = cursor.fetchone()
     close()
     return result
-    
- 
-def get_random_quiz_id():
-    query = 'SELECT quiz_id FROM quiz_content'
-    open()
-    cursor.execute(query)
-    ids = cursor.fetchall()
-    rand_num = randint(0, len(ids) - 1)
-    rand_id = ids[rand_num][0]
-    close()
-    return rand_id
-    
- 
+
+
 def main():
     clear_db()
     create()
     add_questions()
     add_quiz()
-    show_tables()
     add_links()
     show_tables()
-    pass
-    
+    # Вывод в консоль вопроса с id=3, id викторины = 1
+    print(get_question_after(3, 1))
+
+
 if __name__ == "__main__":
     main()
