70de0a13b1d861824f5ac65a9f7d6e592e7df758
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-19 17:46:36 +0600

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,34 +2,30 @@ from random import randint
 from flask import Flask, session, redirect, url_for
 from db_scripts import get_question_after
 
-
-quiz = 0
-last_question = 0
-
 def index():
-   global quiz, last_question
-   quiz = 1
-   last_question = 0
-   return '<a href="/question">Тест на дебелизм</a>'
+   session['quiz'] = 1
+   session['last_question'] = 0
+   return '<a href="/test">Тест</a>'
 
-
-def question():
-   global last_question
-   result = get_question_after(last_question, quiz)
-   if result is None or len(result) == 0:   
-           return redirect(url_for('result'))
+def test():
+   result = get_question_after(session['last_question'], session['quiz'])
+   if result is None or len(result) == 0:
+       return redirect(url_for('result'))
    else:
-       last_question = result[0]
-       return '<h1>' + str(quiz) + '<br>' + str(result) + '</h1>'
+       session['last_question'] = result[0]
+       # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
+       return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
 
 def result():
-   return "<h1>Викторина закончилась</h1>"
+   return "that's all folks!"
 
-
-app = Flask(__name__)  
+# Создаём объект веб-приложения:
+app = Flask(__name__) 
 app.add_url_rule('/', 'index', index)   # создаёт правило для URL '/'
-app.add_url_rule('/question', 'question', question) # создаёт правило для URL '/test'
+app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
 app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
+# Устанавливаем ключ шифрования:
+app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
 
 if __name__ == '__main__':
    # Запускаем веб-сервер:
